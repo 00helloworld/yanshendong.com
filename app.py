235377aefae536f9f302a2a4af92ffc579014198
Yanshen Dong <yanshen0406@gmail.com>
2024-02-19 23:28:37 -0600

    create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'{name} page'
